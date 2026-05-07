50af7a2a746c0088a819e8d6d96dbf62a08562f9
Rodney Osodo <socials@rodneyosodo.com>
2026-05-07 12:41:42 +0300

    feat(example): add Go Wasmtime example
    Signed-off-by: Rodney Osodo <socials@rodneyosodo.com>

---
+++ example/go.mod
@@ -0,0 +1,7 @@
+module rodneyosodo.com/propeller-example
+
+go 1.26.2
+
+require github.com/bytecodealliance/wasmtime-go/v44 v44.0.0
+
+replace github.com/bytecodealliance/wasmtime-go/v44 => ../
---
+++ example/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bytecodealliance/wasmtime-go/v44"
+)
+
+func main() {
+	store := wasmtime.NewStore(wasmtime.NewEngine())
+
+	wasm, err := wasmtime.Wat2Wasm(`
+      (module
+        (import "" "hello" (func $hello))
+        (func (export "run")
+          (call $hello))
+      )
+    `)
+	check(err)
+
+	module, err := wasmtime.NewModule(store.Engine, wasm)
+	check(err)
+
+	item := wasmtime.WrapFunc(store, func() {
+		fmt.Println("Hello from Go!")
+	})
+
+	instance, err := wasmtime.NewInstance(store, module, []wasmtime.AsExtern{item})
+	check(err)
+
+	run := instance.GetFunc(store, "run")
+	if run == nil {
+		panic("not a function")
+	}
+	_, err = run.Call(store)
+	check(err)
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
